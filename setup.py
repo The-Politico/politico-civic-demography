4d5235bfa7767fd72e4a65e3b96a801da76ce82d
Andrew Briz <briz.andrew@gmail.com>
2018-08-23 16:44:33 -0400

    0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="politico-civic-demography",
-    version="0.1.1",
+    version="0.1.2",
     description="",
     url="https://github.com/The-Politico/politico-civic-demography",
     author="POLITICO interactive news",
